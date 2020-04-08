6089a77cc59ef7f02d57f910be8022d5e92080f4
刘世涛 <liushitao@jd.com>
2020-04-08 14:23:34 +0800

    init

---
+++ src/main/java/com/liust/git/FirstGit.java
@@ -0,0 +1,10 @@
+package com.liust.git;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FirstGit {
+    public static void main(String[] args) {
+//        log.error("git, let`s go! ");
+    }
+}
