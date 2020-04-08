f8c7c33ccaf6aa6190cc42cd210ca5d0298f35cd
刘世涛 <liushitao@jd.com>
2020-04-08 14:38:35 +0800

    add log

--- src/main/java/com/liust/git/FirstGit.java
+++ src/main/java/com/liust/git/FirstGit.java
@@ -5,6 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FirstGit {
     public static void main(String[] args) {
-//        log.error("git, let`s go! ");
+        log.error("git, let`s go! ");
     }
 }
