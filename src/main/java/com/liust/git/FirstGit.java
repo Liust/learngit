ba680b2464206659f99c625519c14d3056550ef9
刘世涛 <liushitao@jd.com>
2020-04-08 14:39:58 +0800

    add log two

--- src/main/java/com/liust/git/FirstGit.java
+++ src/main/java/com/liust/git/FirstGit.java
@@ -6,5 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class FirstGit {
     public static void main(String[] args) {
         log.error("git, let`s go! ");
+        log.info("git add log: info ");
     }
 }
